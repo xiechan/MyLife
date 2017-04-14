020f9eb7b14d34e9ea320d5767315adca651ffc1
谢婵 <416728551@qq.com>
2017-04-14 16:46:47 +0800

    d

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,6 +24,7 @@
             <div class="col-sm-12 overflow">
                 <div class="social-icons pull-right">
                     <ul class="nav nav-pills">
+                        <li><a href="http://open.weibo.com/widgets?cat=wb"><i class="fa fa-weibo"></i></a></li>
                         <li><a href=""><i class="fa fa-facebook"></i></a></li>
                         <li><a href=""><i class="fa fa-twitter"></i></a></li>
                         <li><a href=""><i class="fa fa-google-plus"></i></a></li>
